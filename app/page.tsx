0fff4b4ce8384c0e810368ef54a3984f8c3e071a
zain <zainmd.htm@gmail.com>
2024-09-11 22:22:20 +0500

    Weather-Widget App

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+// export default function Home(){
+//     return(
+//         <h1>Hello World</h1>
+//     )
+// }
+
+import WeatherWidget from "@/components/weather-widget";
+                                        
+
+export default function Home() {
+  return (
+    <div>
+      <WeatherWidget />
+    </div>
+  );
+}
